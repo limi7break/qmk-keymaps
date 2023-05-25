c04a2ed8df801eaa5ce85d50d30253b3801d40fc
Michele Ferri <m.ferri@wifisolution.ch>
2023-05-25 12:35:13 +0200

    improved cirque trackpad

--- crkbd/limi7break/rules.mk
+++ crkbd/limi7break/rules.mk
@@ -1,10 +1,13 @@
 EXTRAKEY_ENABLE 	   = yes
+
+OLED_ENABLE            = no
+OLED_DRIVER            = SSD1306
+
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+
 RGBLIGHT_ENABLE        = no
 RGB_MATRIX_ENABLE      = yes
 RGB_MATRIX_CUSTOM_USER = yes
-LTO_ENABLE             = yes
 
-# OLED_ENABLE            = yes
-# OLED_DRIVER            = SSD1306
+LTO_ENABLE             = yes
